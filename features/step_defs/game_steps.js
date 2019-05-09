c5b2de3a0d4b32a2778a97531dd5d1a0f5f54e73
HardPlant <abc7988se@naver.com>
2019-05-09 19:44:16 +0900

    define step

---
+++ features/step_defs/game_steps.js
@@ -0,0 +1,44 @@
+var {Given, When, Then} = require("cucumber");
+var include = require("jsload");
+include("js/Game.js");
+include("js/Node.js");
+
+Given('{string} * {string} 게임판이 초기화된다', function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('{string}이 {string}, {string} 위치에 배치된다', function (string, string2, string3) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('{string}은 랜덤하게 움직인다', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('{string}이 {string}, {string} 위치에 배치된다', function (string, string2, string3) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('{string}은 랜덤하게 움직인다', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('턴이 진행되면', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('{string}이 {string}, {string} 위치에 없다', function (string, string2, string3) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('{string}이 {string}, {string} 위치에 없다', function (string, string2, string3) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
