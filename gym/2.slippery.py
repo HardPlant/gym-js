239e266e1ef5e39c20efdd765157ee5ac8d5a78e
HardPlant <abc7988se@naver.com>
2019-05-12 15:07:35 +0900

    update

---
+++ gym/2.slippery.py
@@ -0,0 +1,74 @@
+import numpy as np
+import random as pr
+import matplotlib.pyplot as plt
+
+import gym
+from gym.envs.registration import register
+
+
+def rargmax(vector):
+    # 가장 큰 값 중 랜덤하게 반환
+    m = np.amax(vector)
+    indices = np.nonzero(vector == m)[0]
+    return pr.choice(indices)
+
+register(
+    id="FrozenLake-v3",
+    entry_point="gym.envs.toy_text:FrozenLakeEnv",
+    kwargs={"map_name" : "4x4"}
+)
+
+env = gym.make("FrozenLake-v0")
+observation = env.reset()
+
+# table to wzro
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+num_episodes = 2000
+
+# 모든 결과의 저장
+rList = []
+for i in range(num_episodes):
+    state = env.reset()
+    rAll = 0
+    done = False
+
+    while not done:
+        
+        # e - greedy
+        # e = 0.1 / ((i // 100) + 1)
+        # if np.random.rand(1) < e:
+        #     action = env.action_space.sample()
+        # else:
+        #     action = rargmax(Q[state, :])
+
+        ## OR
+
+        # decaying
+        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / ( i + 1))
+        
+        #state, reward, done, info
+        new_state, reward, done, _ = env.step(action)
+
+        # discounted future reward
+        dis = 0.99
+        # Q table 추가
+        #Q[state, action] = reward + dis * np.max(Q[new_state, :])
+
+        learning_rate = 0.85
+        Q[state, action] = (1-learning_rate) * Q[state, action] \
+            + learning_rate * (reward + dis * np.max(Q[new_state, :]))
+
+        rAll += reward
+        state = new_state
+    
+    rList.append(rAll)
+
+#print
+
+print("Success rate: " + str(sum(rList)/num_episodes))
+print("Final Q-Table Values")
+print("Left Down Right Up")
+print(Q)
+
+plt.bar(range(len(rList)), rList, color="blue")
+plt.show()
--- gym/RL.md
+++ gym/RL.md
@@ -27,3 +27,67 @@ a = argmax(q(state, action) + random_values / (i+1))
 
 gamma = 0.9
 Q(s,a) = r + gamma * argmax(q(s', a'))
+
+## Deterministic / Stochastic (Non-deterministic)
+
+Stochastic ->
+Q(s') little bit
+update Q(s) little bit(learning rate)
+Many mantor (not one mentor)
+
+## Q-Learning
+
+Q(s,a) = (1-lr)Q(s,a) + lr(r + gamma(argmax(Q(s,a'))))
+
+## Q-Network
+
+(s,a) => q
+를 반환하는 NN
+
+### linear regression
+
+H(x) = Wx
+
+Ws = Qs*
+
+cost(W) = (Ws - y)^2, y = r + gamma*max(Q(s'))
+
+
+W[s] = pred(Q(s,a) | theta) =~ Q&(s, a)
+
+## QN
+
+Q = random(W)
+for episode= 1..M
+    # pi near= s (전처리가 없을 경우)
+    seq[1] = {x[1]}, preprocessed seq pi[1] = pi[s[1]]
+    
+    for t= 1..T
+        E => random_action()
+        Not(E) => maxarg(a, Q(pi), a;theta)
+
+        execute(a) => 
+        set s[t+1] = s[t], a[t], x
+
+y label = if terminal(pi[j+1]) r[j] else r[j] + gamma*maxarg(a', Q(pi[j+1], a';theta))
+loss function => gradient descent step
+(y[j]-Q(pi[j],a[j];theta))^2
+
+* no learning rate (1-lr)(Q(s,a)) + lr*()
+.. gradient descent가 학습을 대신 함
+
+min(theta, sum(t=>0, )
+diverges => Deep, Replay, Separaed Network (DQN)
+
+## 입력과 출력 설계
+
+One-hot .. 1자리만 사용함
+
+one_hot(x)=>{return np.identity(16)[x:x,1]}
+
+state 7 -> one-hot(7)
+np.identify(16)[s1:s1 + 1] # index, [16][1]
+
+in[16] => out[4]
+
+
